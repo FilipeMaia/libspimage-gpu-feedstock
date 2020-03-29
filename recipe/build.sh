8a989a27607812574cf0801dd5d349b02c9de51f
Filipe Maia <filipe.c.maia@gmail.com>
2020-03-30 00:22:26 +0200

    Initial feedstock commit with conda-smithy 3.6.16.dev6.

---
+++ recipe/build.sh
@@ -0,0 +1,16 @@
+
+mkdir build
+cd build
+
+if [[ ${HOST} =~ .*darwin.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+else
+    CMAKE_PLATFORM_FLAGS+=("")
+fi
+
+cmake .. -DUSE_CUDA=OFF \
+	    -DCMAKE_BUILD_TYPE=Release \
+	    -DCMAKE_PREFIX_PATH=${PREFIX} \
+	    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+	    ${CMAKE_PLATFORM_FLAGS[@]} 
+make install

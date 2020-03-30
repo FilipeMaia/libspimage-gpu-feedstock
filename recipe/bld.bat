83ac406ce3c8f15933780ef2f66b019b85174c92
Filipe Maia <filipe.c.maia@gmail.com>
2020-03-30 22:27:24 +0200

    Add proper windows instructions

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,14 @@
-"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
+
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_BUILD_TYPE=Release
+      %SRC_DIR%
+
+:: Build.
+cmake --build . --config Release
 if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config
+if errorlevel 1 exit 1
+

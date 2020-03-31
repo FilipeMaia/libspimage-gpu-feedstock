e4a14bd1ca0d1f06061579c2b17830f9558640a8
Filipe Maia <filipe.c.maia@gmail.com>
2020-03-31 22:54:55 +0200

    Mais uma

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,6 @@ fi
 # CUDA_HOME is defined by nvcc metapackage
 # From: https://github.com/floydhub/dl-docker/issues/59
 cmake    -DUSE_CUDA=ON \      
-         -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME} \
-	 -DCMAKE_LIBRARY_PATH=${CUDA_HOME}/lib64/stubs \
 	 -DCMAKE_BUILD_TYPE=Release \
 	 -DCMAKE_PREFIX_PATH=${PREFIX} \
 	 -DCMAKE_INSTALL_PREFIX=${PREFIX} \

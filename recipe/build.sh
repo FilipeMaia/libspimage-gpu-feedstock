9ed91504b40809785d98df6467cc4cf6cbf9c24c
Filipe Maia <filipe.c.maia@gmail.com>
2020-03-31 21:58:34 +0200

    Trying out some fancy CUDA flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,9 +8,15 @@ else
     CMAKE_PLATFORM_FLAGS+=("")
 fi
 
-cmake .. -DUSE_CUDA=ON \
-	    -DCMAKE_BUILD_TYPE=Release \
-	    -DCMAKE_PREFIX_PATH=${PREFIX} \
-	    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-	    ${CMAKE_PLATFORM_FLAGS[@]} 
+# CUDA_HOME is defined by nvcc metapackage
+CMAKE_FLAGS=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME}"
+# From: https://github.com/floydhub/dl-docker/issues/59
+CMAKE_FLAGS+=" -DCMAKE_LIBRARY_PATH=${CUDA_HOME}/lib64/stubs"
+
+cmake .. -DUSE_CUDA=ON \      
+         ${CMAKE_FLAGS} \
+	 -DCMAKE_BUILD_TYPE=Release \
+	 -DCMAKE_PREFIX_PATH=${PREFIX} \
+	 -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+	 ${CMAKE_PLATFORM_FLAGS[@]} 
 make install
